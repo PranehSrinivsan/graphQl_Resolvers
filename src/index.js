ac4ce2f9b638e4d87fec3bf8ccf82c6009d2fc74
pranehsrinvsan <psrinivasan@fleetstudio.com>
2022-02-23 10:35:05 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,77 @@
+const {ApolloServer,gql} = require('apollo-server')
+
+const typeDefs = gql`
+    type Query{
+        hello(name:String!): String!
+        user: User!
+    }
+
+    type User{
+        id:ID!
+        username: String!
+    }
+
+    type Error{
+        field: String!
+        message: String!
+    }
+
+    type RegisterResponce{
+        error: [Error]
+        user: User
+    }
+
+    input Userinfo {
+        username: String!,
+        password:String!
+    }
+
+    type Mutation{
+        register(userinfo: Userinfo!): RegisterResponce!
+        login(userinfo: Userinfo!): [String]!
+    }
+`;
+
+const resolvers={
+
+    User:{
+        username:(parent)=> `Hi ${parent.username}`,
+        id:(parent)=> parent.id+0 
+    },
+
+    Query:{
+        hello:(parent,{name})=>`Hi ${name}`,
+        user: ()=>({
+            id:1,
+            username:"firstuser"
+        })
+    },
+    Mutation:{
+        register: ()=>({
+            user:{
+                id:1,
+                username:"userone"
+            },
+            error:[
+            {
+                field: "field  of error",
+                message: "error message"
+            },
+            {
+                field: "field  of error",
+                message: "error message"}
+            ]
+        }),
+        login:(parent,{userinfo:{username,password}},context,info)=>{
+            return [username,password]
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context:({req,res})=>({req,res})
+});
+
+server.listen().then(({url})=>console.log('server running at'+url));
